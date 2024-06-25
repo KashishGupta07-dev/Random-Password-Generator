364f5f91d7bc957659e4fb94ebbf4fefe0781390
KashishGupta07-dev <kashishgupta.y2k5@gmail.com>
2024-06-26 02:21:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ function App() {
   function changeHandler(event){
       setPasswordLength(event.target.value);
       document.getElementsByClassName('slider')?.[0].addEventListener('input', function() {
-        const value = this.value;
-        const min = this.min;
-        const max = this.max;
+        const value = event.target.value;
+        const min = event.target.min;
+        const max = event.target.max;
         const percentage = ((value - min) / (max - min)) * 100;
         
         this.style.background = `linear-gradient(to right, violet 0%, violet ${percentage}%, gray ${percentage}%, gray 100%)`;
@@ -71,24 +71,26 @@ function App() {
       <div className='flex flex-col gap-y-2 px-6 py-4 rounded-md w-full'>
           <div className='text-[#ffffff] opacity-75 uppercase tracking-[1.5px] text-center leading-5 text-lg font-bold'>Password Generator</div>
           <form onSubmit={handleSubmit(submitHandler)}>
-          <div className='relative  bg-[#0B0D1B] rounded-md border-b-[0.2rem] border-solid  border-[rgb(80,48,128)] my-[1rem] mx-[0rem] pt-[0.35rem]  flex justify-between'>
+          <div className='mb-[1rem]'>
+          <div className='relative  bg-[#0B0D1B] rounded-md border-b-[0.2rem] border-solid  border-[rgb(80,48,128)] mt-[1rem] mx-[0rem] pt-[0.35rem]  flex justify-between'>
               <input type='text' readOnly={true} name='password' placeholder='PASSWORD' className=" bg-transparent pt-[0.5rem] outline-none cursor-default pb-[1rem] pl-[0.75rem] font-medium text-yellow-500 placeholder:text-yellow-500 placeholder:font-medium placeholder:tracking-[1.3px] tracking-[1.3px]"
                 {
                   ...register("password")
                 }
               />
-              {
-                errors.password &&
-                <span className='text-green-500 mt-1 mb-1 '>{errors.password.message}</span>
-              }
              <FaRegCopy size={"25px"} className={`absolute top-4 mr-2 text-blue-600 right-0 ${getValues().password?.length > 0 ? "cursor-pointer block" : "hidden"}`} onClick={()=>{
               navigator.clipboard.writeText(getValues().password)
               toast.success("Password Copied To Clipboard")
               }}/> 
               </div>
+              {
+                errors.password &&
+                <span className='text-green-500 mb-1 '>{errors.password.message}</span>
+              }
+              </div>
               <div className='bg-[#0B0D1B] rounded-lg px-10 py-4 flex flex-col gap-y-5'>
               <div className='text-white text-lg font-semibold flex justify-between'>{`Password Length :`}<span className='text-yellow-400'>{passwordLength}</span></div>
-              <input type="range" defaultValue={passwordLength} min={5} max={20} onChange={changeHandler} className='slider appearance-none  bg-no-repeat bg-[rgb(80,48,128)] cursor-pointer rounded-[1rem] h-2 focus:outline-none focus:outline-yellow-300'/>
+              <input type="range" defaultValue={passwordLength} min={5} max={20} onInput={changeHandler} className='slider appearance-none  bg-no-repeat bg-[rgb(80,48,128)] cursor-pointer rounded-[1rem] h-2 focus:outline-none focus:outline-yellow-300'/>
               <div className='flex flex-col gap-y-2 items-start mt-4'>
               <div className="flex gap-2">
               <input type='checkbox' name='uppercase' defaultChecked={true} className={` appearance-none w-[12px] h-[12px] border-[1px] border-solid border-cyan-300 cursor-pointer relative rounded-[0.2rem] checked:bg-cyan-500 checked:before:content-['✓'] checked:before:absolute checked:before:top-[-0.35rem] checked:before:left-[-0.04rem] checked:before:font-bold checked:before:text-sm`}
